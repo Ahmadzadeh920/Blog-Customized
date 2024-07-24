75a60b4845e111069caac9b1213ea5a6dca130fa
Ahmadzadeh920 <Ahmadzadeh920@users.noreply.github.com>
2024-07-24 09:40:04 +0330

    commit static files in ngnix

--- Core/Core/settings.py
+++ Core/Core/settings.py
@@ -159,11 +159,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"  # For Deployment
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "mediafiles"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
-]
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
